8911eab5f628406266d829905ad40cc0249b18ef
Craig Ford <craig_ford@icloud.com>
2023-04-07 16:07:49 -0400

    added code to dealersSlice to sort state when adding dealer

--- client/src/features/dealers/dealersSlice.js
+++ client/src/features/dealers/dealersSlice.js
@@ -15,7 +15,23 @@ const dealersSlice = createSlice({
   reducers: {
     dealerAdded(state, action) {
       state.entities.push(action.payload);
-    },
+      let sortArr = state.entities
+      // sort new array by name
+      sortArr.sort((a, b) => {
+        const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      
+        // names must be equal
+        return 0;
+      });
+      state.entities = sortArr
+    },  
     dealerRemoved(state, action) {
       const idx = state.entities.findIndex((dealer) => dealer.id === action.payload.id);
       state.entities.splice(idx, 1);
